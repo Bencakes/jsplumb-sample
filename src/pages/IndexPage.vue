daa5a63ac12092513fd4ff1fc2d56063a2203fc2
Bencakes <810014486@qq.com>
2024-03-08 10:28:22 +0800

    安装jsplumb；初始化基础页面

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,67 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
+    <div class="canvas">
+      <div class="rectangle-node" ref="node1" style="top: 100px; left: 100px">
+        节点1
+      </div>
+
+      <div class="rectangle-node" ref="node2" style="top: 100px; left: 400px">
+        节点2
+      </div>
+    </div>
+    <!-- <example-component
       title="Example component"
       active
       :todos="todos"
       :meta="meta"
-    ></example-component>
+    ></example-component> -->
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+// import { Todo, Meta } from 'components/models';
+// import ExampleComponent from 'components/ExampleComponent.vue';
 import { ref } from 'vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+// const todos = ref<Todo[]>([
+//   {
+//     id: 1,
+//     content: 'ct1'
+//   },
+//   {
+//     id: 2,
+//     content: 'ct2'
+//   },
+//   {
+//     id: 3,
+//     content: 'ct3'
+//   },
+//   {
+//     id: 4,
+//     content: 'ct4'
+//   },
+//   {
+//     id: 5,
+//     content: 'ct5'
+//   }
+// ]);
+// const meta = ref<Meta>({
+//   totalCount: 1200
+// });
 </script>
+
+<style lang="scss">
+.canvas {
+  position: relative;
+  width: 1000px;
+  height: 800px;
+  border: 1px solid #696868ba;
+
+  .rectangle-node {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    border: 1px solid #000000;
+  }
+}
+</style>
