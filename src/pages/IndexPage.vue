d8f0a309143963cb0e882338faaf50c5235f3086
Bencakes <810014486@qq.com>
2024-03-08 12:01:37 +0800

    创建节点、锚点、连线、Overlay的基本演示

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,53 +1,95 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div class="canvas">
-      <div class="rectangle-node" ref="node1" style="top: 100px; left: 100px">
+    <div class="canvas" ref="canvas">
+      <div
+        class="rectangle-node items-center justify-center row"
+        ref="node1"
+        style="top: 100px; left: 100px"
+      >
         节点1
       </div>
 
-      <div class="rectangle-node" ref="node2" style="top: 100px; left: 400px">
+      <div
+        class="rectangle-node items-center justify-center row"
+        ref="node2"
+        style="top: 100px; left: 400px"
+      >
         节点2
       </div>
+
+      <div
+        class="rectangle-node items-center justify-center row"
+        ref="node3"
+        style="top: 300px; left: 100px"
+      >
+        节点3
+      </div>
+
+      <div
+        class="rectangle-node items-center justify-center row"
+        ref="node4"
+        style="top: 300px; left: 400px"
+      >
+        节点4
+      </div>
     </div>
-    <!-- <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component> -->
   </q-page>
 </template>
 
 <script setup lang="ts">
-// import { Todo, Meta } from 'components/models';
-// import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import {
+  newInstance,
+  BrowserJsPlumbInstance,
+  AnchorLocations,
+} from '@jsplumb/browser-ui';
 
-// const todos = ref<Todo[]>([
-//   {
-//     id: 1,
-//     content: 'ct1'
-//   },
-//   {
-//     id: 2,
-//     content: 'ct2'
-//   },
-//   {
-//     id: 3,
-//     content: 'ct3'
-//   },
-//   {
-//     id: 4,
-//     content: 'ct4'
-//   },
-//   {
-//     id: 5,
-//     content: 'ct5'
-//   }
-// ]);
-// const meta = ref<Meta>({
-//   totalCount: 1200
-// });
+const canvas = ref<HTMLElement>();
+const node1 = ref<Element>(Object());
+const node2 = ref<Element>(Object());
+const node3 = ref<Element>(Object());
+const node4 = ref<Element>(Object());
+
+const jsPlumb = ref<BrowserJsPlumbInstance>();
+
+onMounted(() => {
+  jsPlumb.value = newInstance({
+    container: canvas.value,
+  });
+
+  const endpoint3 = jsPlumb.value.addEndpoint(node3.value, {
+    endpoint: {
+      type: 'Dot',
+      options: {
+        radius: 4,
+      },
+    },
+    anchor: [AnchorLocations.Right],
+  });
+
+  const endpoint4 = jsPlumb.value.addEndpoint(node4.value, {
+    endpoint: {
+      type: 'Dot',
+      options: {
+        radius: 4,
+      },
+    },
+    anchor: [AnchorLocations.Left],
+  });
+
+  jsPlumb.value.connect({
+    source: node1.value,
+    target: node2.value,
+    connector: 'Flowchart',
+  });
+
+  jsPlumb.value.connect({
+    source: endpoint3,
+    target: endpoint4,
+    connector: 'Flowchart',
+    overlays: [{ type: 'Arrow', options: { location: 1 } }],
+  });
+});
 </script>
 
 <style lang="scss">
